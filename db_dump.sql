50804a8d553558f3f5e2b9ef1ea25d07ccbdc7b5
Asmajati Ananto <87751322+asmajati15@users.noreply.github.com>
2023-02-15 11:29:47 +0700

    Add files via upload

--- db_dump.sql
+++ db_dump.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 08, 2023 at 07:37 AM
+-- Generation Time: Feb 15, 2023 at 05:29 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.0.25
 
@@ -30,8 +30,8 @@ SET time_zone = "+00:00";
 CREATE TABLE `history_lelang` (
   `id_history` int(11) NOT NULL,
   `id_lelang` int(11) NOT NULL,
-  `id_user` int(11) NOT NULL,
-  `penawaran_harga` int(20) NOT NULL
+  `id_user` int(11) DEFAULT NULL,
+  `penawaran_harga` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -39,8 +39,27 @@ CREATE TABLE `history_lelang` (
 --
 
 INSERT INTO `history_lelang` (`id_history`, `id_lelang`, `id_user`, `penawaran_harga`) VALUES
-(1, 1, 1, 110),
-(2, 2, 1, 120);
+(1, 2, NULL, 2000000),
+(2, 3, NULL, 15000),
+(3, 1, NULL, 15000),
+(4, 7, NULL, 11000),
+(5, 7, NULL, 12000);
+
+--
+-- Triggers `history_lelang`
+--
+DELIMITER $$
+CREATE TRIGGER `INSERTTTTT` AFTER INSERT ON `history_lelang` FOR EACH ROW UPDATE tb_lelang SET harga_akhir = NEW.penawaran_harga WHERE id_lelang = NEW.id_lelang AND status = 1
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `VALIDATEEE` BEFORE INSERT ON `history_lelang` FOR EACH ROW IF NEW.penawaran_harga > (SELECT harga_akhir FROM tb_lelang WHERE id_lelang = NEW.id_lelang) THEN
+    UPDATE tb_lelang SET tb_lelang.harga_akhir = new.penawaran_harga WHERE tb_lelang.id_lelang = NEW.id_lelang AND STATUS=1;
+    ELSE
+    SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Price must be greater than the previous record.';
+  END IF
+$$
+DELIMITER ;
 
 -- --------------------------------------------------------
 
@@ -50,19 +69,40 @@ INSERT INTO `history_lelang` (`id_history`, `id_lelang`, `id_user`, `penawaran_h
 
 CREATE TABLE `tb_barang` (
   `id_barang` int(11) NOT NULL,
-  `nama_barang` varchar(25) NOT NULL,
+  `nama_barang` varchar(255) NOT NULL,
   `tgl` date NOT NULL,
-  `harga_awal` int(20) NOT NULL,
-  `deskripsi_barang` varchar(100) NOT NULL,
-  `status_barang` int(1) NOT NULL
+  `harga_awal` int(11) NOT NULL,
+  `deskripsi_barang` text NOT NULL,
+  `status_barang` int(11) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `tb_barang`
 --
 
-INSERT INTO `tb_barang` (`id_barang`, `nama_barang`, `tgl`, `harga_awal`, `deskripsi_barang`, `status_barang`) VALUES
-(1, 'Wizard Storm', '2023-02-10', 100, 'test', 1);
+INSERT INTO `tb_barang` (`id_barang`, `nama_barang`, `tgl`, `harga_awal`, `deskripsi_barang`, `status_barang`, `created_at`, `updated_at`) VALUES
+(1, 'CANDI BOROBUDUR', '2023-02-16', 12000, 'apa weee', 1, NULL, NULL),
+(2, 'CANDI KESEMEK', '2023-02-17', 1000000, 'APAAJAAA', 1, NULL, NULL),
+(3, 'CANDI PRAMBANAN', '2023-02-18', 12000, 'SHFUHSU', 1, NULL, NULL),
+(5, 'CANDI SUKUH', '2023-02-17', 1000, 'AHFAJ', 1, '2023-02-14 20:34:56', '2023-02-14 20:34:56'),
+(6, 'CANDI CETHO', '2023-02-18', 15000, 'SHNFJA', 1, '2023-02-14 20:42:21', '2023-02-14 20:42:21'),
+(7, 'aa', '2023-02-15', 10000, 'as', 0, '2023-02-14 21:16:26', '2023-02-14 21:18:29');
+
+--
+-- Triggers `tb_barang`
+--
+DELIMITER $$
+CREATE TRIGGER `INSERTTT` AFTER INSERT ON `tb_barang` FOR EACH ROW INSERT INTO tb_lelang (id_barang,tgl_lelang,harga_akhir,id_user,id_petugas,status) VALUES(NEW.id_barang,NULL,NEW.harga_awal,0,NULL,1)
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `update` AFTER UPDATE ON `tb_barang` FOR EACH ROW UPDATE tb_lelang
+SET tb_lelang.status = NEW.status_barang
+WHERE tb_lelang.id_barang = NEW.id_barang
+$$
+DELIMITER ;
 
 -- --------------------------------------------------------
 
@@ -73,34 +113,24 @@ INSERT INTO `tb_barang` (`id_barang`, `nama_barang`, `tgl`, `harga_awal`, `deskr
 CREATE TABLE `tb_lelang` (
   `id_lelang` int(11) NOT NULL,
   `id_barang` int(11) NOT NULL,
-  `tanggal_barang` date NOT NULL,
-  `harga_akhir` int(20) NOT NULL,
-  `id_user` int(11) NOT NULL,
-  `id_petugas` int(11) NOT NULL,
-  `status_lelang` int(1) NOT NULL
+  `tgl_lelang` date DEFAULT NULL,
+  `harga_akhir` int(11) DEFAULT NULL,
+  `id_user` int(11) DEFAULT NULL,
+  `id_petugas` int(11) DEFAULT NULL,
+  `status` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `tb_lelang`
 --
 
-INSERT INTO `tb_lelang` (`id_lelang`, `id_barang`, `tanggal_barang`, `harga_akhir`, `id_user`, `id_petugas`, `status_lelang`) VALUES
-(1, 1, '2023-02-08', 110, 1, 1, 1),
-(2, 1, '2023-02-08', 120, 1, 1, 1);
-
---
--- Triggers `tb_lelang`
---
-DELIMITER $$
-CREATE TRIGGER `insert_lelang` AFTER INSERT ON `tb_lelang` FOR EACH ROW INSERT INTO history_lelang (id_lelang, id_user, penawaran_harga) VALUES (NEW.id_lelang, NEW.id_user, NEW.harga_akhir)
-$$
-DELIMITER ;
-DELIMITER $$
-CREATE TRIGGER `status_lelang` AFTER UPDATE ON `tb_lelang` FOR EACH ROW UPDATE tb_barang 
-SET tb_barang.status_barang = NEW.status_lelang
-WHERE tb_barang.id_barang = NEW.id_barang
-$$
-DELIMITER ;
+INSERT INTO `tb_lelang` (`id_lelang`, `id_barang`, `tgl_lelang`, `harga_akhir`, `id_user`, `id_petugas`, `status`) VALUES
+(1, 1, NULL, 15000, 1, NULL, 1),
+(2, 2, NULL, 2000000, 1, NULL, 1),
+(3, 3, NULL, 15000, 1, NULL, 1),
+(5, 5, NULL, 1000, 0, NULL, 1),
+(6, 6, NULL, 15000, 0, NULL, 1),
+(7, 7, NULL, 11000, 0, NULL, 0);
 
 -- --------------------------------------------------------
 
@@ -110,10 +140,10 @@ DELIMITER ;
 
 CREATE TABLE `tb_masyarakat` (
   `id_user` int(11) NOT NULL,
-  `nama_lengkap` varchar(25) NOT NULL,
-  `username` varchar(25) NOT NULL,
-  `password` varchar(25) NOT NULL,
-  `telp` varchar(25) NOT NULL
+  `nama_lengkap` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `telp` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -121,7 +151,7 @@ CREATE TABLE `tb_masyarakat` (
 --
 
 INSERT INTO `tb_masyarakat` (`id_user`, `nama_lengkap`, `username`, `password`, `telp`) VALUES
-(1, 'Megumin', 'mengumin', '123', '123');
+(1, 'Panjul', 'panjul', '$2a$12$RIG2azwhpfmwFFVlA7rvouZGYv6a38jIKrw1vasrRXR2fNHTkYpaK', '123');
 
 --
 -- Indexes for dumped tables
@@ -132,7 +162,7 @@ INSERT INTO `tb_masyarakat` (`id_user`, `nama_lengkap`, `username`, `password`, 
 --
 ALTER TABLE `history_lelang`
   ADD PRIMARY KEY (`id_history`),
-  ADD KEY `id_lelang` (`id_lelang`),
+  ADD KEY `id_lelang` (`id_lelang`,`id_user`),
   ADD KEY `id_user` (`id_user`);
 
 --
@@ -146,9 +176,8 @@ ALTER TABLE `tb_barang`
 --
 ALTER TABLE `tb_lelang`
   ADD PRIMARY KEY (`id_lelang`),
-  ADD KEY `id_barang` (`id_barang`),
-  ADD KEY `id_user` (`id_user`),
-  ADD KEY `id_petugas` (`id_petugas`);
+  ADD KEY `id_barang` (`id_barang`,`id_user`,`id_petugas`),
+  ADD KEY `id_user` (`id_user`);
 
 --
 -- Indexes for table `tb_masyarakat`
@@ -164,43 +193,25 @@ ALTER TABLE `tb_masyarakat`
 -- AUTO_INCREMENT for table `history_lelang`
 --
 ALTER TABLE `history_lelang`
-  MODIFY `id_history` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_history` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `tb_barang`
 --
 ALTER TABLE `tb_barang`
-  MODIFY `id_barang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_barang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `tb_lelang`
 --
 ALTER TABLE `tb_lelang`
-  MODIFY `id_lelang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_lelang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `tb_masyarakat`
 --
 ALTER TABLE `tb_masyarakat`
   MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `history_lelang`
---
-ALTER TABLE `history_lelang`
-  ADD CONSTRAINT `history_lelang_ibfk_1` FOREIGN KEY (`id_lelang`) REFERENCES `tb_lelang` (`id_lelang`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `history_lelang_ibfk_2` FOREIGN KEY (`id_user`) REFERENCES `tb_masyarakat` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `tb_lelang`
---
-ALTER TABLE `tb_lelang`
-  ADD CONSTRAINT `tb_lelang_ibfk_1` FOREIGN KEY (`id_barang`) REFERENCES `tb_barang` (`id_barang`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `tb_lelang_ibfk_2` FOREIGN KEY (`id_user`) REFERENCES `tb_masyarakat` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
